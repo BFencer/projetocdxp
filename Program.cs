48ba1d9a291f1a85a81a20fec2ef503704366b96
FIC <lqa.gabriel@gmail.com>
2017-10-05 21:02:21 -0300

    primeiro commit vamo

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ namespace ConsoleCadastro
         static void Main(string[] args)
         {
             Console.WriteLine("Olá Mundo!");
+            FazCafe("chocolate");
+        }
+
+        static void FazCafe(string strSabor)
+        {
+            System.Console.WriteLine("poe agua");
+            System.Console.WriteLine("poe cafe");
+            System.Console.WriteLine("Poe a capsula de " + strSabor);
         }
     }
 }
